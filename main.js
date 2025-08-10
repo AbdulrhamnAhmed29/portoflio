d4b6c0f285ea02799160bc5da6938d6efb52d3de
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2025-08-10 07:58:49 -0700

    وصف التعديل اللي عملته

---
+++ main.js
@@ -0,0 +1,9 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed('#typed', {
+      strings: ['Front-End Developer', 'Web Designer', 'React Developer '],
+      typeSpeed: 70,   
+      backSpeed: 30,   
+      backDelay: 2000,  
+      loop: true        
+    });
+  });
