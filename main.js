b5f45d60a55fb988cb01bc6a738117e70ce6e0ec
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2025-08-11 18:34:11 -0700

    اكتب هنا رسالة توضح التعديلات اللي عملتها

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-  document.addEventListener("DOMContentLoaded", function () {
-    var typed = new Typed('#typed', {
-      strings: ['Front-End Developer', 'Web Designer', 'React Developer '],
-      typeSpeed: 70,   
-      backSpeed: 30,   
-      backDelay: 2000,  
-      loop: true        
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed('#typed', {
+    strings: ['Front-End Developer', 'Web Designer', 'React Developer '],
+    typeSpeed: 70,
+    backSpeed: 30,
+    backDelay: 2000,
+    loop: true
   });
+});
+
+
